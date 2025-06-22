211c95a3d74120df8ccf2cae42507ae5872056a8
rahul9890 <rkumbhar911@gmail.com>
2025-06-22 17:39:43 +0530

    added pitest configs

--- src/main/java/com/example/spring_with_react/filters/CorrelationIdFilter.java
+++ src/main/java/com/example/spring_with_react/filters/CorrelationIdFilter.java
@@ -4,12 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Component
+@Order(1)
 public class CorrelationIdFilter implements Filter {
     private static final Logger logger = LoggerFactory.getLogger(CorrelationIdFilter.class);
     private static final String CORRELATION_ID_HEADER = "X-Correlation-ID";
